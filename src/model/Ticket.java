4dc5bd874e870d94b8bdcb204d97b8b71f1d9b79
waiizhun12 <waiizhun12@gmail.com>
2026-07-09 17:43:15 +0800

    edited Ticket.java

--- src/model/Ticket.java
+++ src/model/Ticket.java
@@ -1,5 +1,61 @@
 package model;
 
 public class Ticket {
+	
+	private String ticketID;
+	private String passenger;
+	private String source;
+	private String destination;
+	private String ticketType;
+	private String status;
+	private double fare;
+	
+	public String getTicketID()
+	{
+		return ticketID;
+	}
+	
+	public String getPassenger()
+	{
+		return passenger;
+	}
+	
+	public String getSourse()
+	{
+		return source;
+	}
+	
+	public String getDestination()
+	{
+		return destination;
+	}
+	
+	public String getTicketType()
+	{                               
+		return ticketType;
+	}
+	
+	public String getStatus()
+	{
+		return status;
+	}
+	
+	public double getFare()
+	{
+		return fare;
+	}
+	
+	public Ticket(String aTicketID, String aPassenger,String aSource, 
+				  String aDestination, String aTicketType, String aStatus, double aFare)
+	{
+		ticketID = aTicketID;
+		passenger = aPassenger;
+		source = aSource;
+		destination = aDestination;
+		ticketType = aTicketType;
+		status = aStatus;
+		fare = aFare;
+	}
 
 }
+
