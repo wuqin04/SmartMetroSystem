e16f4fbbd32d2133453f50f72acd562c1dca0fb0
teozenson <teozenson@gmail.com>
2026-07-12 18:00:30 +0800

    Testing FileM and TXT

--- src/repository/FileManager.java
+++ src/repository/FileManager.java
@@ -1,5 +1,8 @@
 package repository;
 
-public class FileManager {
+public interface FileManager {
+	void saveData(Object data, String fileName);
+	Object loadData(String FileName);
+	
 
 }
--- src/repository/TXTFileManager.java
+++ src/repository/TXTFileManager.java
@@ -1,5 +1,27 @@
 package repository;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-public class TXTFileManager {
+public class TXTFileManager implements FileManager {
+
+	@Override
+	public void saveData(Object data, String fileName) {
+		try {
+			PrintWriter writer = new PrintWriter(new FileWriter(fileName));
+			
+			writer.println("S001,KL Sentral, KL");
+			writer.println("S002,Bangsar, KL");
+			
+			writer
+		}
+		
+	}
+
+	@Override
+	public Object loadData(String FileName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
