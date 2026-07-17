c5a6f55be3f892df3ec2eb2f2495b659e8a08cd5
mingx <mingx@LAPTOP-JR015BO8>
2026-07-17 21:30:05 +0800

    ?

--- src/model/Station.java
+++ src/model/Station.java
@@ -1,15 +1,28 @@
 //display info at ui
 package model;
-
+import java.util.Scanner;
 import service.TicketService;
 
 public class Station {
+	
 	private String stationId;
 	private String name;
 	private String location;
 	
 	public void displayInfo() {
-		System.out.println("");
+		int choice;
+		Scanner sc= new Scanner(System.in);		
+		System.out.println("This is station portal. What do you want to do? (Enter 1, 2 or 3 for station's options or 99 to exit.");
+		System.out.println("1. Add station");
+		System.out.println("2. Search station");
+		System.out.println("3. View station");
+		
+		choice=sc.nextInt();
+		switch (choice) {
+		case 1:
+			
+		}
+		
 	}
 	
 
