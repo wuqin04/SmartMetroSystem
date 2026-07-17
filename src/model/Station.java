f9d4262b491c1074da95618a3f3d67948dc6d268
mingx <mingx@LAPTOP-JR015BO8>
2026-07-17 19:58:47 +0800

    fked

--- src/model/Station.java
+++ src/model/Station.java
@@ -1,6 +1,16 @@
-//view station infos
+//display info at ui
 package model;
 
+import service.TicketService;
+
 public class Station {
+	private String stationId;
+	private String name;
+	private String location;
+	
+	public void displayInfo() {
+		System.out.println("");
+	}
+	
 
 }
--- src/service/StationService.java
+++ src/service/StationService.java
@@ -1,5 +1,20 @@
 package service;
+import java.util.Scanner;
 
 public class StationService {
-
+	
+	private ArrayList<Station> stations;
+	Scanner sc= new Scanner(System.in);
+	
+	public void addStation() {
+		System.out.println("Insert new station ID: ");
+		//unknown ID var.=sc.nextLine();
+		
+		System.out.println("Insert the new station's name: ");
+		//unknown name var.=sc.nextLine();
+		
+		System.out.println("Insert the new station's location: ");
+		//unknown loc var.=sc.nextLine();
+	}
+	
 }
