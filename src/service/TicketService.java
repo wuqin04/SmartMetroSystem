b79286613ebcec98094ca322d4733c2c8757e42c
waiizhun12 <waiizhun12@gmail.com>
2026-07-20 22:22:19 +0800

    edited TicketService

--- src/service/TicketService.java
+++ src/service/TicketService.java
@@ -1,12 +1,14 @@
 package service;
 import java.util.ArrayList;
 import enums.TicketType;
+import enums.UserRole;
 import fare.FareCalculator;
 import fare.StandardFareCalculator;
 import model.Ticket;
 import model.Passenger;
 import model.Route;
 import model.Station;
+import model.User;
 import exception.TicketNotFoundException;
 
 
@@ -55,37 +57,74 @@ public class TicketService {
 		
 		// Add the ticket into the ArrayList
 		tickets.add(ticket);
+		System.out.printf("[SUCCESS]: Ticket purchase successful! Remaining Balance: RM%.2f\n", passenger.getBalance());
 		
 		return ticket;
 	}
 	
-	public void cancelTicket(String ticketId) throws TicketNotFoundException {
+	public void cancelTicket(String ticketId, Passenger passenger) throws TicketNotFoundException {
 		
 		if(ticketId == null || ticketId.trim().isEmpty()) {
 			throw new IllegalArgumentException("[ERROR]: Ticket ID cannot be null or empty.");
 		}
 		
-		for (int i = 0; i < tickets.size(); i++) {
-			Ticket ticket = tickets.get(i);
-			
-			if(ticket.getTicketId().equalsIgnoreCase(ticketId.trim())) {
-				 ticket.cancelTicket();
-				 return;
-			}
-			throw new TicketNotFoundException("[ERROR]: Ticket ID not found.");
+		if(passenger == null){
+			throw new IllegalArgumentException("[ERROR]: Passenger cannot be null.");
 		}
-	}
-	
-	public void viewTicket() {
 		
-		if(tickets.isEmpty()) {
-			System.out.println("[ERROR]: No tickets found.");
-			return;
-		}
+		String enteredId = ticketId.trim();
 		
 		for(int i = 0; i < tickets.size(); i++) {
-			System.out.println("-------------------------------\n");
-			tickets.get(i).printTicket();
+			Ticket ticket = tickets.get(i);
+			
+			if(ticket.getTicketId().equalsIgnoreCase(enteredId)) {
+				
+				if(ticket.getPassenger().getUserId().equals(passenger.getUserId())) {
+					ticket.cancelTicket();
+					return;
+				}
+				throw new IllegalArgumentException("[ERROR]: Ticket does not belongs to passenger" + passenger.getUserId() + ".");
+			}
+		}
+		throw new TicketNotFoundException("[ERROR]: Ticket ID not found.");
+	}
+	
+	public void viewTickets(User user) {
+		
+		if(user == null) {
+			throw new IllegalArgumentException("[ERROR]: User cannot be null.");
+		}
+		
+		if(user.getRole() == UserRole.ADMIN) {
+			
+			if(tickets.isEmpty()) {
+				throw new IllegalArgumentException("[ERROR]: No ticket found.");
+			}
+			
+			for(int i = 0; i < tickets.size(); i++) {
+				Ticket ticket = tickets.get(i);
+				ticket.printTicket();
+			}
+		} else if(user.getRole() == UserRole.PASSENGER ) {
+			
+			boolean ticketFound = false;
+	
+			for(int i = 0; i < tickets.size(); i++) {
+				
+				Ticket ticket = tickets.get(i);
+				
+				if(ticket.getPassenger().getUserId().equals(user.getUserId())){
+					 ticket.printTicket();
+					 ticketFound = true;
+				}
+			}
+			
+			if(!ticketFound) {
+				throw new IllegalArgumentException("[ERROR]: Passenger has no tickets.");
+			} 
+			
+		} else {
+			throw new IllegalArgumentException("[ERROR]: Invalid user role.");
 		}
 	}
 }
