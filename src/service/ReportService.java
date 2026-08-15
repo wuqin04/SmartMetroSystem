a4108f800fe9d1baaaf603dd5c84acc900f9f2c4
teozenson <teozenson@gmail.com>
2026-08-15 23:28:56 +0800

    Rearrange report services

--- src/service/ReportService.java
+++ src/service/ReportService.java
@@ -18,40 +18,39 @@ public class ReportService {
         this.tickets = tickets;
     }
 
-//Identify the sold tickets from the array list
-public void showTotalSales() {
-	int count = 0;
-	for(Ticket t: tickets) {
-		if(t.getStatus() != TicketStatus.CANCELLED) {
-			count++;
-		}
-	}
-	System.out.println("Total tickets sold: " + count);
-}
+    //Identify the sold tickets from the array list
+    public void showTotalSales() {
+    	int count = 0;
+    	for(Ticket t: tickets) {
+    		if(t.getStatus() != TicketStatus.CANCELLED) {
+    			count++;
+    		}
+    	}
+    	System.out.println("Total tickets sold: " + count);
+    }
 
-//add up the revenue based on sold ticket
-public void showTotalRevenue() {
-	double revenue = 0.0;
-	for(Ticket t: tickets) {
-		if(t.getStatus() != TicketStatus.CANCELLED) {
-			revenue += t.getFare();
-		}
-	}
-	System.out.printf("Total revenue: RM %.2f%n", revenue);
-}
+    //add up the revenue based on sold ticket
+    public void showTotalRevenue() {
+    	double revenue = 0.0;
+    	for(Ticket t: tickets) {
+    		if(t.getStatus() != TicketStatus.CANCELLED) {
+    			revenue += t.getFare();
+    		}
+    	}
+    	System.out.printf("Total revenue: RM %.2f%n", revenue);
+    }	
 
-//List out the cancelled tickets from the array list
-public void showCancelledTickets() {
-	boolean found = false;
-	for(Ticket t: tickets) {
-		if(t.getStatus() == TicketStatus.CANCELLED) {
-			System.out.println("Cancelled Ticket ID: " + t.getTicketId());
-            found = true;
+    //List out the cancelled tickets from the array list
+    public void showCancelledTickets() {
+    	boolean found = false;
+    	for(Ticket t: tickets) {
+    		if(t.getStatus() == TicketStatus.CANCELLED) {
+    			System.out.println("Cancelled Ticket ID: " + t.getTicketId());
+    			found = true;
+				}
 			}
-		}
-		if(found != true) {
-		System.out.println("No Cancelled Tickets.");
-		}
-	
-	}
+			if(found != true) {
+				System.out.println("No Cancelled Tickets.");
+			}
+	}	
 }
